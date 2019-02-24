156095b13b941ab387d89ae56607c9235274772d
bidpapa <nengomashe@gmail.com>
2019-02-24 12:16:09 +0300

    Позиционирование мод. окон, замена кавычек, новый код слайдера

--- js/scripts.js
+++ js/scripts.js
@@ -4,40 +4,40 @@ if (window.location.toString().indexOf('index.html') > 0) {
 
   /* Контактная форма */
 
-  var contactPopup = document.getElementById("modal-contact");
-  var contactFormRef = document.getElementById("company-contacts-ref");
-  var contactForm = document.querySelector(".contact-form");
-  var nameUser = document.getElementById("contact-form-nameuser");
-  var emailUser = document.getElementById("contact-form-emailuser");
-  var messageUser = document.getElementById("contact-form-letteruser");
+  var contactPopup = document.getElementById('modal-contact');
+  var contactFormRef = document.getElementById('company-contacts-ref');
+  var contactForm = document.querySelector('.contact-form');
+  var nameUser = document.getElementById('contact-form-nameuser');
+  var emailUser = document.getElementById('contact-form-emailuser');
+  var messageUser = document.getElementById('contact-form-letteruser');
   var isStorageSupport = true;
-  var storage = "";
+  var storage = '';
 
   try {
-    storageNameUser = localStorage.getItem("nameUser");
-    storageEmailUser = localStorage.getItem("emailUser");
+    storageNameUser = localStorage.getItem('nameUser');
+    storageEmailUser = localStorage.getItem('emailUser');
   } catch (err) {
     isStorageSupport = false;
   }
 
-  contactForm.addEventListener("submit", function(evt) {
+  contactForm.addEventListener('submit', function(evt) {
     if (!nameUser.value || !emailUser.value || !messageUser.value) {
       evt.preventDefault();
       console.log('Не все поля заполнены!');
-      contactPopup.classList.remove("modal-error");
+      contactPopup.classList.remove('modal-error');
       void contactPopup.offsetWidth;
-      contactPopup.classList.add("modal-error");
+      contactPopup.classList.add('modal-error');
     }
     else if (isStorageSupport) {
-      localStorage.setItem("nameUser", nameUser.value);
-      localStorage.setItem("emailUser", emailUser.value);
+      localStorage.setItem('nameUser', nameUser.value);
+      localStorage.setItem('emailUser', emailUser.value);
     }
   });
 
-  contactFormRef.addEventListener("click", function(evt) {
+  contactFormRef.addEventListener('click', function(evt) {
     evt.preventDefault();
-    document.getElementById("modal-contact").style.display="block";
-    contactPopup.classList.add("modal-appearence");
+    document.getElementById('modal-contact').style.display='block';
+    contactPopup.classList.add('modal-appearence');
     if (storageNameUser && storageEmailUser) {
       nameUser.value = storageNameUser;
       emailUser.value = storageEmailUser;
@@ -51,57 +51,57 @@ if (window.location.toString().indexOf('index.html') > 0) {
 
   /* Карта */
 
-  var mapModal = document.getElementById("company-contacts-map-ref");
-  mapModal.addEventListener("click", function(evt) {
+  var mapModal = document.getElementById('company-contacts-map-ref');
+  mapModal.addEventListener('click', function(evt) {
     evt.preventDefault();
-    document.getElementById("modal-map").style.display="block";
+    document.getElementById('modal-map').style.display='block';
   });
 
   /* Добавление товара в корзину */
 
-  var items = document.querySelectorAll(".buy");
+  var items = document.querySelectorAll('.buy');
   for(var i = 0; i < items.length; ++i)
   {
-    items[i].addEventListener("click", function(evt) {
+    items[i].addEventListener('click', function(evt) {
     evt.preventDefault();
-    document.getElementById("modal-basket").style.display="block";
+    document.getElementById('modal-basket').style.display='block';
   });
   }
 
   /* Кнопки закрытия форм */
 
-  document.getElementById("reset-basket").onclick = function() {resetBasket()};
+  document.getElementById('reset-basket').onclick = function() {resetBasket()};
   function resetBasket() {
-    document.getElementById("modal-basket").style.display="none";
+    document.getElementById('modal-basket').style.display='none';
   }
 
-  document.getElementById("reset-contact").onclick = function() {resetContacts()};
+  document.getElementById('reset-contact').onclick = function() {resetContacts()};
   function resetContacts() {
-    document.getElementById("modal-contact").style.display="none";
-    contactPopup.classList.remove("modal-error");
+    document.getElementById('modal-contact').style.display='none';
+    contactPopup.classList.remove('modal-error');
   }
 
-  document.getElementById("reset-map").onclick = function() {resetMap()};
+  document.getElementById('reset-map').onclick = function() {resetMap()};
   function resetMap() {
-    document.getElementById("modal-map").style.display="none";
+    document.getElementById('modal-map').style.display='none';
   }
 
   /* Закрытие по esc */
 
-  window.addEventListener("keydown", function (evt) {
+  window.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
-      var allModals = document.querySelectorAll(".modal");
+      var allModals = document.querySelectorAll('.modal');
       for (var i = 0; i < allModals.length; i++) {
-        allModals[i].style.display="none";
-        contactPopup.classList.remove("modal-error");
+        allModals[i].style.display='none';
+        contactPopup.classList.remove('modal-error');
       }
     }
   });
 
   /* Слайдер на главной */
 
-  var firstDot = document.querySelector('.slider-dot:first-child');
+  /* var firstDot = document.querySelector('.slider-dot:first-child');
   var secondDot = document.querySelector('.slider-dot:last-child');
 
   if (document.querySelector('.sld2').classList.contains('inactive')) {
@@ -119,37 +119,79 @@ if (window.location.toString().indexOf('index.html') > 0) {
     for (var i = 0; i < 2; i++) {
     Dots[i].classList.toggle('active-dot');
     }
+  } */
+
+  var Slides = document.querySelectorAll('.offers-slide');
+  var Dots = document.querySelectorAll('.slider-dot');
+  var btnBack = document.querySelector('.slider-back-button');
+  var btnForward = document.querySelector('.slider-forward-button');
+  var Slider = document.querySelector('.offers-slider');
+  numberDots = Dots.length;
+  numberSlides = Slides.length;
+
+  /* Кнопка Назад */
+
+  btnBack.addEventListener('click', function(evt) {
+    evt.preventDefault();
+
+    for (var i = 0 ; i < Slides.length; i++)
+    {
+      if (!Slides[i].classList.contains('inactive')) {
+        Slides[i].classList.toggle('inactive');
+        Dots[i].classList.toggle('active-dot');
+        if (i == 0) {
+          Slides[numberSlides - 1].classList.toggle('inactive');
+          Dots[Dots.length - 1].classList.toggle('active-dot');
+        }
+        else {
+          Slides[i - 1].classList.toggle('inactive');
+          Dots[i - 1].classList.toggle('active-dot');
+        }
+        break;
+      }
+    }
+
+  });
+
+  /* Кнопка Вперед */
+
+  btnForward.addEventListener('click', function(evt) {
+    evt.preventDefault();
+
+    for (var i = 0 ; i < Slides.length; i++)
+    {
+      if (!Slides[i].classList.contains('inactive')) {
+        Slides[i].classList.toggle('inactive');
+        Dots[i].classList.toggle('active-dot');
+        if (i == (numberSlides - 1)) {
+          Slides[0].classList.toggle('inactive');
+          Dots[0].classList.toggle('active-dot');
+        }
+        else {
+          Slides[i + 1].classList.toggle('inactive');
+          Dots[i + 1].classList.toggle('active-dot');
+        }
+        break;
+      }
+    }
+
+  });
+
+  /* Точки-переключатели */
+
+  for (var i = 0; i < numberDots; i++) {
+    Dots[i].addEventListener('click', set_handler(i));
   }
 
-  var btnBack = document.querySelector('.slider-back-button');
-  btnBack.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    document.querySelector('.sld1').classList.toggle('inactive');
-    document.querySelector('.sld2').classList.toggle('inactive');
-    dotToggle();
-  });
-
-  var btnForward = document.querySelector('.slider-forward-button');
-  btnForward.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    document.querySelector('.sld1').classList.toggle('inactive');
-    document.querySelector('.sld2').classList.toggle('inactive');
-    dotToggle();
-  });
-
-  firstDot.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    document.querySelector('.sld2').classList.add('inactive');
-    document.querySelector('.sld1').classList.remove('inactive');
-    dotToggle();
-  });
-
-  secondDot.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    document.querySelector('.sld1').classList.add('inactive');
-    document.querySelector('.sld2').classList.remove('inactive');
-    dotToggle();
-  });
+  function set_handler(i){
+  return function () {
+    console.log(i);
+    document.querySelector('.active-dot').classList.remove('active-dot');
+    Dots[i].classList.add('active-dot');
+    Slider.querySelector('div:not(.inactive)').classList.add('inactive');
+    Slides[i].classList.remove('inactive');
+  };
+  }
 
 }
 
@@ -159,30 +201,30 @@ else {
 
   /* Добавление товара в корзину */
 
-  var items = document.querySelectorAll(".buy");
+  var items = document.querySelectorAll('.buy');
   for(var i = 0; i < items.length; ++i)
   {
-    items[i].addEventListener("click", function(evt) {
+    items[i].addEventListener('click', function(evt) {
     evt.preventDefault();
-    document.getElementById("modal-basket").style.display="block";
+    document.getElementById('modal-basket').style.display='block';
   });
   }
 
   /* Кнопки закрытия форм */
 
-  document.getElementById("reset-basket").onclick = function() {resetBasket()};
+  document.getElementById('reset-basket').onclick = function() {resetBasket()};
   function resetBasket() {
-    document.getElementById("modal-basket").style.display="none";
+    document.getElementById('modal-basket').style.display='none';
   }
 
   /* Закрытие по esc */
 
-  window.addEventListener("keydown", function (evt) {
+  window.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
-      var allModals = document.querySelectorAll(".modal");
+      var allModals = document.querySelectorAll('.modal');
       for (var i = 0; i < allModals.length; i++) {
-        allModals[i].style.display="none";
+        allModals[i].style.display='none';
       }
     }
   });
